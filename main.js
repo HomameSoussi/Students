b5a3ae826c1cd37c112599effe2245537301e86e
HomameSoussi <121523106+HomameSoussi@users.noreply.github.com>
2023-04-11 23:36:16 +0000

    Create main.js

---
+++ main.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const searchForm = document.getElementById("searchForm");
+    const bookSummary = document.getElementById("bookSummary");
+
+    searchForm.addEventListener("submit", async (e) => {
+        e.preventDefault();
+
+        const bookTitle = document.getElementById("bookTitle").value;
+        const authorName = document.getElementById("authorName").value;
+
+        const prompt = `Write a thorough yet concise summary of **${bookTitle}**. Concentrate on only the most important takeaways and primary points from the book that together will give me a solid overview and understanding of the book and its topic. \n\nInclude all of the following in your summary: \n\n- Main topic or theme of the book\n- Key ideas or arguments presented\n- Chapter titles or main sections of the book with a paragraph on each\n- Key takeaways or conclusions\n- Author's background and qualifications\n- Comparison to other books on the same subject\n- Target audience or intended readership\n- Reception or critical response to the book\n- Publisher and First Published Date\n- Recommendations [Other similar books on the same topic]\n\nTo sum up:  **The book's biggest takeaway and point in a singular sentence**\n\n## Main topic or theme\n\n- \n\n## Key ideas or arguments presented\n\n- \n\n## Chapter titles or main sections of the book\n\n### \n\n- \n\n## Key takeaways or conclusions\n\n- \n\n## Author's background and qualifications\n\n- \n\n## Comparison to other books on the same subject\n\n- \n\n## Target audience or intended readership\n\n- \n\n## Reception or critical response to the book\n\n- \n\n## Publisher and First Published Date\n\n- \n\n## Recommendations\n\n- `;
+        
+        const summary = await getBookSummary(prompt);
+        bookSummary.innerHTML = summary;
+    });
+});
+
+async function getBookSummary(prompt) {
+    const openaiApiKey = "sk-XXX";
+    const openaiOrg = "org-XX";
+
+    const response = await fetch("https://api.openai.com/v1/engines/davinci-codex/completions", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${openaiApiKey}`,
+            "OpenAI-Organization": openaiOrg,
+        },
+        body: JSON.stringify({
+            prompt: prompt,
+            n: 1,
+            max_tokens: 300,
+            stop: null,
+        }),
+    });
+
+    const data = await response.json();
+    return data.choices[0].text;
+}
