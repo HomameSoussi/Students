ecd12167dd7d4b4cd0b01c7a56a10bb9f3364da5
HomameSoussi <121523106+HomameSoussi@users.noreply.github.com>
2023-04-12 00:34:19 +0000

    Update main.js

--- main.js
+++ main.js
@@ -15,15 +15,22 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-async function getBookSummary(prompt) {
-  const response = await fetch("/api/gpt-proxy", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ prompt }),
-  });
-
-  const data = await response.json();
-  return data.summary;
+async function getBookSummary() {
+  const bookTitle = document.getElementById('bookTitle').value;
+  const authorName = document.getElementById('authorName').value;
+  const prompt = `Write a thorough yet concise summary of ${bookTitle} by ${authorName}. Concentrate on only the most important takeaways and primary points from the book that together will give me a solid overview and understanding of the book and its topic. \n\nInclude all of the following in your summary: \n\n- Main topic or theme of the book\n- Key ideas or arguments presented\n- Chapter titles or main sections of the book with a paragraph on each\n- Key takeaways or conclusions\n- Author's background and qualifications\n- Comparison to other books on the same subject\n- Target audience or intended readership\n- Reception or critical response to the book\n- Publisher and First Published Date\n- Recommendations [Other similar books on the same topic]\n\nTo sum up:  **The book's biggest takeaway and point in a singular sentence**\n\n## Main topic or theme\n\n- \n\n## Key ideas or arguments presented\n\n- \n\n## Chapter titles or main sections of the book\n\n### \n\n- \n\n## Key takeaways or conclusions\n\n- \n\n## Author's background and qualifications\n\n- \n\n## Comparison to other books on the same subject\n\n- \n\n## Target audience or intended readership\n\n- \n\n## Reception or critical response to the book\n\n- \n\n## Publisher and First Published Date\n\n- \n\n## Recommendations\n\n- `;
+  const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ prompt })
+  };
+  try {
+    const response = await fetch('/api/gpt-proxy', requestOptions);
+    const data = await response.json();
+    document.getElementById('summary').value = data;
+  } catch (error) {
+    console.error('Error:', error);
+  }
 }
+
+
