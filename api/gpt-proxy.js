8de9da91a1b6a323a876414f7bd18458bbe42777
HomameSoussi <121523106+HomameSoussi@users.noreply.github.com>
2023-04-12 00:08:48 +0000

    Update gpt-proxy.js

--- api/gpt-proxy.js
+++ api/gpt-proxy.js
@@ -1,23 +1,26 @@
-const fetch = require("node-fetch");
 
 module.exports = async (req, res) => {
-  const { prompt } = req.body;
-  const openaiApiKey = process.env.OPENAI_API_KEY;
+  const { bookTitle } = req.body;
 
-  const response = await fetch("https://api.openai.com/v1/engines/davinci-codex/completions", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization": `Bearer ${openaiApiKey}`,
-    },
-    body: JSON.stringify({
-      prompt: prompt,
-      n: 1,
-      max_tokens: 300,
-      stop: null,
-    }),
-  });
+  const prompt = `Write a thorough yet concise summary of **${bookTitle}**. Concentrate on only the most important takeaways and primary points from the book that together will give me a solid overview and understanding of the book and its topic. \n\nInclude all of the following in your summary: \n\n- Main topic or theme of the book\n- Key ideas or arguments presented\n- Chapter titles or main sections of the book with a paragraph on each\n- Key takeaways or conclusions\n- Author's background and qualifications\n- Comparison to other books on the same subject\n- Target audience or intended readership\n- Reception or critical response to the book\n- Publisher and First Published Date\n- Recommendations [Other similar books on the same topic]\n\nTo sum up:  **The book's biggest takeaway and point in a singular sentence**\n\n## Main topic or theme\n\n- \n\n## Key ideas or arguments presented\n\n- \n\n## Chapter titles or main sections of the book\n\n### \n\n- \n\n## Key takeaways or conclusions\n\n- \n\n## Author's background and qualifications\n\n- \n\n## Comparison to other books on the same subject\n\n- \n\n## Target audience or intended readership\n\n- \n\n## Reception or critical response to the book\n\n- \n\n## Publisher and First Published Date\n\n- \n\n## Recommendations\n\n-`;
 
-  const data = await response.json();
-  res.status(200).json({ summary: data.choices[0].text });
+  try {
+    const completion = await openai.ChatCompletion.create({
+      model: "gpt-3.5-turbo",
+      messages: [
+        { role: "user", content: prompt },
+      ],
+    });
+
+    if (completion.choices && completion.choices[0] && completion.choices[0].message) {
+      const result = completion.choices[0].message.content.strip();
+      res.status(200).json({ summary: result });
+    } else {
+      console.error('Error: Unexpected response from OpenAI API:', completion);
+      res.status(500).json({ error: 'Unexpected response from OpenAI API' });
+    }
+  } catch (error) {
+    console.error('Error:', error.message);
+    res.status(500).json({ error: error.message });
+  }
 };
